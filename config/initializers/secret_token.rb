21571f3bbfd73c9f4f41443f17884f9a3ffc4398
ipshow <ipshow@gmail.com>
2013-03-28 16:49:59 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baiche::Application.config.secret_token = 'bf9516b052dc70452af485a95dd5f11427734b40e4cc8d39a61d33d2462bd98c80decc75133fb9944e70ce7474af3e019823fc901ad44aa2ecd4ed78781c6421'
